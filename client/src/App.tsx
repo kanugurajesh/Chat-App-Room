48f6961684d4a508e65f8731646ea4c49d518fa8
kanugurajesh <kanugurajesh3@gmail.com>
2024-04-21 01:02:28 +0530

    Added the chat input and submit button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,42 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 function App() {
+  const socket = io("http://localhost:3000");
+  const [message, setMessage] = useState<string>("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("Cilcked on submit");
+    socket.emit("message", message);
+  };
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected", socket.id);
+    });
+    socket.on("welcome", (s) => {
+      console.log(s);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="h-screen w-screen items-center justify-center flex">
+      <div className="flex gap-4">
+        <input
+          type="text"
+          placeholder="Enter the message"
+          name=""
+          id=""
+          onChange={(e) => setMessage(e.target.value)}
+          className="border-2 border-black rounded-md pl-2 "
+        />
+        <button onClick={handleSubmit} className="bg-blue-700 text-white font-bold p-2 px-3 rounded-md border-2 border-blue-700 hover:text-blue-700 hover:bg-white transition-all duration-300 ease-in-out">Submit</button>
+      </div>
     </div>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
-
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,7 +8,14 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 const server = createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin:"http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
@@ -17,9 +23,17 @@ app.get("/", (req: Request, res: Response) => {
 
 io.on("connection", (socket) => {
   console.log("User Connected");
-  console.log("Id", socket.id)
+  console.log("Id", socket.id);
+  
+  socket.on("message", (data) => {
+    console.log(data);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  })
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
