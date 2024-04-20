4ee6ef450ac0b007a6763945f5f08902909d2c1c
kanugurajesh <kanugurajesh3@gmail.com>
2024-04-21 00:08:25 +0530

    Added tailwind to client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,24 @@
-// src/index.js
 import express, { Express, Request, Response } from "express";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+
+io.on("connection", (socket) => {
+  console.log("User Connected");
+  console.log("Id", socket.id)
+})
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
